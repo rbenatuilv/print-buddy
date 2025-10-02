cae5b9867e90175fc883a27351c7f97f6f19a50e
rbenatuilv <rbenatuilv@uc.cl>
2025-10-02 21:56:15 +0200

    feat: creation of user model and CRUD

---
+++ src/db/crud/user.py
@@ -0,0 +1,53 @@
+from sqlmodel import Session, select
+
+from ..models.user import User
+from ...schemas.user import UserCreate
+
+
+class UserService:
+
+
+   ########################## CREATE #########################
+
+    def create_user(
+        self, 
+        user_data: UserCreate,
+        session: Session
+    ) -> User:
+        
+        user = User(**user_data.model_dump())
+
+        session.add(user)
+        session.commit()
+
+        return user
+    
+    ########################## READ ##########################
+
+    def email_exists(
+        self,
+        email: str,
+        session: Session 
+    ) -> bool:
+
+        statement = select(User).where(User.email == email)
+        user = session.exec(statement).first()
+        return user is not None
+
+    def username_exists(
+        self,
+        username: str,
+        session: Session
+    ):
+        return self.get_user_by_username(username, session) is not None
+
+    def get_user_by_username(
+        self,
+        username: str,
+        session: Session
+    ) -> User | None:
+
+        statement = select(User).where(User.username == username)
+        user = session.exec(statement).first()
+
+        return user
---
+++ src/db/main.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, create_engine, Session
+from ..core.config import settings
+
+
+engine = create_engine(settings.DB_URL, echo=True)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
