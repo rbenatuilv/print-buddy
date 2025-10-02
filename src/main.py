e488e6ab3033bdd38fc7d6ceff0fe59b5c8c9b34
rbenatuilv <rbenatuilv@uc.cl>
2025-10-02 21:59:27 +0200

    feat: app base

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from .core.config import settings
+from .api import router
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION
+)
+
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=[settings.URL, "127.0.0.1", "localhost"]
+)
+
+app.include_router(router)
+
+
