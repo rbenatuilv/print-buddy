bcffd957b89250a2f44fe739262b5e50bfda3427
rbenatuilv <rbenatuilv@uc.cl>
2025-10-03 15:50:55 +0200

    fix: avoid unnecessary continuous queries to the db

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from sqlmodel import Session, select
+import json
 
 from .cups import CUPSManager
 from ..db.main import engine
@@ -15,9 +16,30 @@ class Scheduler(AsyncIOScheduler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.printers_data = []
+
+    def _normalize_data(self, printers_data: list[dict]) -> str:
+        return json.dumps(printers_data, sort_keys=True)
+
+    def check_printer_changes(self, printers_data: list[dict]) -> bool:
+        if not self.printers_data:
+            self.printers_data = printers_data
+            return True
+
+        old_state = self._normalize_data(self.printers_data)
+        new_state = self._normalize_data(printers_data)
+
+        if old_state != new_state:
+            self.printers_data = printers_data
+            return True
+
+        return False
 
     def update_printers(self):
         printers_data = cups_mgr.get_printers()
+
+        if not self.check_printer_changes(printers_data):
+            return
 
         with Session(engine) as session:
             for data in printers_data:
@@ -29,5 +51,5 @@ class Scheduler(AsyncIOScheduler):
                 )
 
     def start(self, *args, **kwargs):
-        self.add_job(self.update_printers, "interval", seconds=10)
+        self.add_job(self.update_printers, "interval", seconds=60)
         super().start(*args, **kwargs)
