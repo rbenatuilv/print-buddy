5f3dc97fe1e9ab57c8fde3d705179bdb75abe457
rbenatuilv <rbenatuilv@uc.cl>
2025-10-21 10:32:02 +0200

    fix: added cups.sock to work with docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,9 @@ git pull
 docker build -t print-buddy .
 docker stop print-buddy 2>/dev/null || true
 docker rm print-buddy 2>/dev/null || true
-docker run -d -p 8000:8000 --name print-buddy print-buddy
+docker run -d \
+    -p 8000:8000 \
+    --name print-buddy \
+    -v /var/run/cups/cups.sock:/var/run/cups/cups.sock \
+    print-buddy
 echo "✅ Backend redeployed!"
